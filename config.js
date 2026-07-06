d4d1891b5785dbad093df11720779c5031508423
vicinity6 <justvibes@tutamail.com>
2026-07-06 22:31:21 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Matchday Manager Cloud config
-// Use your Supabase Project URL and Publishable key.
-
-const SUPABASE_URL = "https://ufaycuunqtygpcflebof.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_tIrDrqrl7rK6YxZ94mvD_A_cnO3oqMo";
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
